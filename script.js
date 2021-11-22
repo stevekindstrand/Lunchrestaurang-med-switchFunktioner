11d5b2ac8364ac5ca978fb60ad7f13743d35ca57
Steve Kindstrand <stevekindstrand@gmail.com>
2021-11-22 14:35:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+whichDay = days[0]; 
+
+switch(whichDay) {
+    case "Monday":
+        console.log(whichDay + ":" + " Pasta Alfredo");
+    break;
+    case "Tuesday":
+        console.log(whichDay + ":" + " Oven baked chicken and rice");
+    break;
+    case "Wednesday":
+        console.log(whichDay + ":" + " Baked potatoes with meatballs");
+    break;
+    case "Thursday":
+        console.log(whichDay + ":" + " Pancakes");
+    break;
+    case "Friday":
+        console.log(whichDay + ":" + " Tacos");
+    break;
+    case "Saturday":
+        console.log(whichDay + ":" + " Closed");
+    break;
+    case "Sunday":
+        console.log(whichDay + ":" + " Closed");
+    break;
+}
